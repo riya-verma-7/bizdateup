c4c10f98b3abee830355ad2e7c55b20130439354
entremax <100113074+entremax@users.noreply.github.com>
2022-03-28 18:05:45 +0530

    Commit

--- src/pages/Startups/Details.js
+++ src/pages/Startups/Details.js
@@ -132,76 +132,7 @@ duediligence: "File",
         founderimg: Founder,
         linkedin: 'https://www.instagram.com/jeetmchandan/',
     },
-{
-    id: '3',
-    title: "Try Startup",
-        img: Img2,
-        logo: Logo,
-        description: 'Bidwheelz is a B2B and B2C transparent auction aggregator platform which helps people buy and sell vehicles through AI inspection models. The company provides all different types of services related to automobiles such as Sale, Purchase, Insurance, Loan, Servicing and Accessories which a customer would require during their vehicle use under one roof.',
-        shortdescription: 'Bidwheelz is a Marketplace for buying and selling resale vehicles through Transparent Live Bidding.',
-        percentageRaised: '20%', 
-        tag: [
-          "Automobile","E-Commerce",     
-        ],
-        youtubelink: "https://youtu.be/UVCP4bKy9Iw",
-         icon: 'mdi-account-group',
-        icon2: 'mdi-lightbulb-on',
-        icon3: 'mdi-email-lock',
-        green: 'mdi-checkbox-marked-circle',
-        due: 'mdi-file-download',
-        facebook: "https://www.facebook.com/",
-        twitter: "https://www.twitter.com/",
-        gmail: "https://www.facebook.com/",
-        instagram: "https://www.instagram.com/",
-        file: Download2,
-        
 
-
-        duediligence: "File",
-        keyfeature1: "Done 1770 auction i", 
-        
-        keyfeature2: "Signed up 350 dealers i",
-        
-        keyfeature3: "Pan India Contract of ICICI ",
-        
-        keyfeature4: "Scalable",
-
-    // pitchdeck
-
-    // Yaha pitch ke jagah pe details hai
-
-    problem: "Let us all agree on this, Fitness can be BORING! Users: How many times have we set clear intentions like the following: This year I am going to go to the gym regularly! This year onwards I am going to run regularly! From now on, fitness and eating healthy is going to be my top-most priority only to realize two weeks later (for 92% of us at least) we have fallen back to our old ways. Fitness is 'BORING' and that is one of the largest reasons why most of us drop out of fitness habits. Users are rarely hooked on to the same fitness programs. It is not easy to come up with unique programs for everyone, especially not at a low cost. Brands: Brands have always been looking for ways to engage both internal and external users with campaigns that can put them in the positive mindspace of their users. Health related events have a very positive angle to it, however it has NOT been easy to create, coordinate, market and get reports around these from one single platform.",
-    solution: "TELO - an events-based fitness tracking app for all of us! 'Events' however have the ability to keep things light, fun and engaging, especially when there is the possibility of it being a social and community driven phenomenon. Enter TELO, a mobile app where users can discover, register, set-reminders, practice/train, participate/track, share and brag achievements and more importantly - Build a Tribe around their fitness habits! Imagine a day where you have grown fit, but also have built a community around your friends circle, family, school,college, social media or anywhere you find your tribe. After all, we humans are all social animals!",
-    product: "TELO is meant to be a platform app where users can discover fun and engaging fitness based events around them. Besides that they would have the ability to: Register for free/paid events; Set reminders for the same on their calendar; Train for events if they choose to; Participate in group events hosted in and around their localities; Track the fitness events like Runs, Walks and Jogs with metrics like - distance, pace/speed, elevation, calories burnt, steps covered at various intervals - daily, weekly, monthly, yearly and all time; Track your own achievements and if necessary compare yourself with others in your community/groups to better your performance; Track their fitness activities with smart tracking device integrations like Apple Watch, FitBit, Google Wear etc. (coming soon); Fun audio tracks (gamified story format) that help you achieve your running and other fitness goals that act like guides; Share achievements and build a community of like minded people within your community; Many more to come soon…",
-    traction: "Aug 2020 - Having started working on the product in Aug 2020, the first private beta was launched in Dec 2020 to test if the idea even has legs. Within two weeks, we hit a 300+ user mark and the idea of audio stories that help running and achieving a set goal was validated to a certain degree; Jan 2021 - From Dec 2020 to Jan 2021, we continued the private beta and got some validation and feedback while working on the better version of the product. In Jan 2021, we released the open beta version of TELO which is what is still live with some updates; Traction until Oct 2021 (peak numbers): Downloads - 25000+ MAUs - 5100+ WAUs - 1600+ Pivot identified after speaking with users. Although the concept of an Audio-AR driven model to get people excited about running/walking/jogging, it did NOT lead to repeat usage of the product. It was sporadic and it became evident that the vanity of such a product is not long lasting and we needed a stronger value proposition for it to be a household daily used product on our users’ phones. Which is when the repeated mention of fun short events started peaking in user conversations. This is what has lead to the pivot and rebuild of TELO into a platform for small sized events in smaller location circles and giving users the ability to create groups/communities too.",
-    customers: "Pivoting from a B2C model to a B2B2C model now and hence have only begun building the product, while speaking to some really interesting strategic partnerships with premium sports apparel brands, smartphone brands, property developers/builders who want to be part of the journey, can’t name them as yet as the conversations are still preliminary. Having said that, there is surely interest in this segment. Strategic partnership would be with the brands like Milind Soman and his branded event - Pinkathon. As mentioned in the video, he is ready to be a brand ambassador if the proposition is right.",
-    businessmodel: "B2B2C model where brands participate in creating fun and engaging experiences around fitness and all of them have to be tracked on the app itself. Building the much needed momentum and not all customer acquisition has to be done by us. Brands would promote events they have sponsored on TELO in order to create buzz around how they care about their users’ fitness/health overall. Brands would pay TELO to host the events and in exchange they would get the analytics, reporting and additional support in creating fun and engaging audio and visual content that can be placed strategically in the app that could play during the event in the best suitable format - video, audio, reading material etc. A simple example here for a Property Developer Brands create ‘Free’ events - Pay $ for analytics and reports; Brands create ‘Paid’ events - Pay higher $ for analytics, reports and the majority portion of the entry fees paid by users.TELO takes a cut; Brands pay $ for smartly placed advertisements within events wherever feasible and non-intrusive for users; Users pay $ for participating in paid events, TELO earns a cut out of it; B2C model also exists but will be the last focus where users can be incentivised to pay a monthly subscription to be able to participate in a max number of paid events for free and earn points and other goodies provided by brands for their participation; Users can also pay for training modules created by experts and coaches out of which a majority would go to the respective coaches/experts and TELO takes a small cut",
-    competition: "TELO would be competing in two areas with two different kinds of entities - Fitness tracking and Events. The following video and attached image should help illustrate this",
-    fundsusage: "Main usage of funds would be to build two key components for success - a great team and a kicka** product. We intend to build a world class product from India for the world. Built in a manner that entering different geographies would be light and not a reinvention every time. The rough breakup of usage of the funds are mentioned here in the illustration below with some key milestones we hope to achieve with the funding:",
-    vision: "gdrthth",
-    exit: "thtjyj",
-    link: "www.internkhoj.com",
-
-    // fundamentals
-    
-    minimuminvest: '5000',
-    valuation: '57846666',
-    target: '57846666',
-    socials: [
-        "fjebof", 'efheofuhne'
-    ],
-
-    
-    foundername: 'Nakul Khandelwal',
-    foundernamerole: 'Co-founder and CEO',
-    founderdesc: 'Hotel Management graduate with 3 years of hospitality experience at Hotel Royal Orchid.',
-
-    foundername1: 'Sameer Tilak',
-    foundernamerole1: 'Co-founder and CTO',
-    founderdesc1: 'Hotel Management graduate with exposure to on ground business management having worked for 2 years',
-    founderimg: Founder,
-    linkedin: 'https://www.instagram.com/jeetmchandan/',
-}
 
     
     
